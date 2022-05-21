282b74e3879ed087427b8c3c21445f246030e12f
ArmindoFlores <francisco.rodrigues0908@gmail.com>
2022-05-21 22:11:01 +0100

    Separated code into relevant files

---
+++ auxiliar_scripts/landmark_extractor.py
@@ -0,0 +1,99 @@
+import math
+import random
+
+import numpy as np
+
+
+def to_cartesian(theta, r):
+    if r > 0.001:
+        return r * math.cos(theta), r * math.sin(theta)
+    else:
+        return 500 * math.cos(theta), 500 * math.sin(theta)
+
+def closest_to_line(x, y, m, b):
+    return (((x + m * y) - m * b) / (m**2 + 1), (m * (x + m * y) + b) / (m**2 + 1))
+
+def extract_features(ls, N=400, C=22, X=0.02, D=10):
+    features = []
+    D = int(round(math.radians(D) / ls["angle_increment"])) 
+    S = 6
+    rsize = len(ls["ranges"])
+    cartesian = tuple(to_cartesian(ls["angle_min"] + i * ls["angle_increment"], r) for i, r in enumerate(ls["ranges"]))
+    available = [i for i in range(rsize) if ls["ranges"][i] > 0.001]
+    
+    # Pre-allocate lists
+    total = [None] * 360
+    total_size = 0
+    cartesian_sample_x = [0] * 360
+    cartesian_sample_y = [0] * 360
+    cartesian_sample_size = 0
+    close_enough = [None] * 360
+    close_enough_size = 0
+    
+    n = 0
+    while n < N and len(available) and rsize > C:
+        n += 1
+        
+        # Select a random reading
+        R = random.choice(available)
+        
+        # Sample S readings withn D degrees of R
+        total_size = 0
+        for i in available:
+            if (R - i) % rsize <= D:
+                total[total_size] = i
+                total_size += 1
+        
+        if total_size < S:
+            continue
+        sample = random.sample(total[:total_size], S)
+        sample.append(R)
+        
+        # Transform these points to cartesian coordinates and compute a least squares best fit line
+        cartesian_sample_size = 0
+        for i in sample:
+            x, y = cartesian[(i - D) % rsize]
+            cartesian_sample_x[cartesian_sample_size] = x
+            cartesian_sample_y[cartesian_sample_size] = y
+            cartesian_sample_size += 1
+            
+        A = np.vstack([cartesian_sample_x[:cartesian_sample_size], np.ones(cartesian_sample_size)]).T
+        m, b = np.linalg.lstsq(A, cartesian_sample_y[:cartesian_sample_size], rcond=None)[0]
+        
+        # Find all readings within X meters of the line
+        d = 1 / pow(m*m + 1, 0.5)
+        close_enough_size = 0
+        for i, (x, y) in enumerate(cartesian):
+            if abs(m * x - y + b) * d < X:
+                close_enough[close_enough_size] = i
+                close_enough_size += 1
+        
+        # If the number of points close to the line is above a threshold C
+        if close_enough_size > C:
+            # Calculate new least squares best fit line
+            A = np.vstack([[cartesian[i][0] for i in close_enough[:close_enough_size]], np.ones(close_enough_size)]).T
+            m, b = np.linalg.lstsq(A, np.array([cartesian[i][1] for i in close_enough[:close_enough_size]]), rcond=None)[0]
+            
+            line_points = sorted((closest_to_line(*cartesian[i], m, b) for i in close_enough[:close_enough_size]), key=lambda i: i[0])
+            features.append((m, b, (line_points[0], line_points[-1])))
+            
+            for point in close_enough[:close_enough_size]:
+                try:
+                    available.remove(point)
+                except ValueError:
+                    pass
+    return features
+
+def extract_landmarks(ls):
+    features = extract_features(ls)
+    landmarks = []
+    for m, b, (start, end) in features:
+        landmark_pos = np.array(closest_to_line(0, 0, m, b))  
+        isnew = True
+        for *_, landmark in landmarks:
+            if np.linalg.norm(landmark - landmark_pos) < 0.25:
+                isnew = False
+                break
+        if isnew:
+            landmarks.append((m, b, (start, end), landmark_pos))
+    return landmarks
--- auxiliar_scripts/visualizer.py
+++ auxiliar_scripts/visualizer.py
@@ -1,100 +1,18 @@
+import math
 import os
 import pickle
-import random
 import sys
 import time
-import cProfile, pstats, io
-from pstats import SortKey
 
 import matplotlib.pyplot as plt
 import numpy as np
 
+import landmark_extractor
 import loader
 
 SAMPLE = "corredor-16-maio"
 SCANS_DIR = os.path.join("laser-scans", SAMPLE)
 ODOM_DIR = os.path.join("odometry", SAMPLE)
-
-
-def to_cartesian(theta, r):
-    if r > 0.001:
-        return r * np.cos(theta), r * np.sin(theta)
-    else:
-        return 500 * np.cos(theta), 500 * np.sin(theta)
-
-def closest_to_line(x, y, m, b):
-    return (((x + m * y) - m * b) / (m**2 + 1), (m * (x + m * y) + b) / (m**2 + 1))
-
-def extract_features(ls, N=400, C=20, X=0.02, D=9):
-    features = []
-    D = int(round(np.radians(D) / ls["angle_increment"])) 
-    S = 6
-    rsize = len(ls["ranges"])
-    cartesian = tuple(to_cartesian(ls["angle_min"] + i * ls["angle_increment"], r) for i, r in enumerate(ls["ranges"]))
-    available = [i for i in range(rsize) if ls["ranges"][i] > 0.001]
-    
-    # Pre-allocate lists
-    total = [None] * 360
-    total_size = 0
-    cartesian_sample_x = [0] * 360
-    cartesian_sample_y = [0] * 360
-    cartesian_sample_size = 0
-    close_enough = [None] * 360
-    close_enough_size = 0
-    
-    n = 0
-    while n < N and len(available) and rsize > C:
-        n += 1
-        
-        # Select a random reading
-        R = random.choice(available)
-        
-        # Sample S readings withn D degrees of R
-        total_size = 0
-        for i in available:
-            if (R - i) % rsize <= D:
-                total[total_size] = i
-                total_size += 1
-        
-        if total_size < S:
-            continue
-        sample = random.sample(total[:total_size], S)
-        sample.append(R)
-        
-        # Transform these points to cartesian coordinates and compute a least squares best fit line
-        cartesian_sample_size = 0
-        for i in sample:
-            x, y = cartesian[(i - D) % rsize]
-            cartesian_sample_x[cartesian_sample_size] = x
-            cartesian_sample_y[cartesian_sample_size] = y
-            cartesian_sample_size += 1
-            
-        A = np.vstack([cartesian_sample_x[:cartesian_sample_size], np.ones(cartesian_sample_size)]).T
-        m, b = np.linalg.lstsq(A, cartesian_sample_y[:cartesian_sample_size], rcond=None)[0]
-        
-        # Find all readings within X meters of the line
-        d = 1 / pow(m*m + 1, 0.5)
-        close_enough_size = 0
-        for i, (x, y) in enumerate(cartesian):
-            if abs(m * x - y + b) * d < X:
-                close_enough[close_enough_size] = i
-                close_enough_size += 1
-        
-        # If the number of points close to the line is above a threshold C
-        if close_enough_size > C:
-            # Calculate new least squares best fit line
-            A = np.vstack([[cartesian[i][0] for i in close_enough[:close_enough_size]], np.ones(close_enough_size)]).T
-            m, b = np.linalg.lstsq(A, np.array([cartesian[i][1] for i in close_enough[:close_enough_size]]), rcond=None)[0]
-            
-            line_points = sorted((closest_to_line(*cartesian[i], m, b) for i in close_enough[:close_enough_size]), key=lambda i: i[0])
-            features.append((m, b, (line_points[0], line_points[-1])))
-            
-            for point in close_enough[:close_enough_size]:
-                try:
-                    available.remove(point)
-                except ValueError:
-                    pass
-    return features
         
 
 def main(t="ls", save=False):
@@ -120,26 +38,16 @@ def main(t="ls", save=False):
                 img[int(y)][int(x)] = 0
             
             start_time = time.time()
-            features = extract_features(scan_info)
+            landmarks = landmark_extractor.extract_landmarks(scan_info)
             end_time = time.time()
             print(f"Time taken: {round((end_time - start_time) * 1000, 2)}ms")
-            landmarks = []
-            for m, b, (start, end) in features:
+            
+            for _, _, (start, end), landmark_pos in landmarks:
                 start, end = np.array(start), np.array(end)
                 start = start * scale_factor + offset
                 end = end * scale_factor + offset
-                landmark_pos = np.array(closest_to_line(0, 0, m, b))
-                
-                isnew = True
-                for landmark in landmarks:
-                    if np.linalg.norm(landmark - landmark_pos) < 0.2:
-                        isnew = False
-                        break
-                    
-                if isnew:
-                    landmarks.append(landmark_pos)
-                    plt.plot([start[0], end[0]], [start[1], end[1]], "r")
-                    plt.plot(*(landmark_pos * scale_factor + offset), "bo")
+                plt.plot([start[0], end[0]], [start[1], end[1]], "r")
+                plt.plot(*(landmark_pos * scale_factor + offset), "bo")
                     
             plt.imshow(img, cmap="gray", interpolation="nearest")#, extent=(-3, 3, -3, 3))
             plt.xlim([0, img.shape[0]])
