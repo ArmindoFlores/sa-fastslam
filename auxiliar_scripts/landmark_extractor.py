be3cac7e09edbd3a8a99b876c7edc6ba2b90ba7a
ArmindoFlores <francisco.rodrigues0908@gmail.com>
2022-05-24 21:02:10 +0100

    Removed unused code

--- auxiliar_scripts/landmark_extractor.py
+++ auxiliar_scripts/landmark_extractor.py
@@ -8,11 +8,6 @@ import numpy as np
 def closest_to_line(x, y, a, b, c):
     return ((b * (b * x - a * y) - a * c) / (a**2 + b**2), (a * (-b * x + a * y) - b * c) / (a**2 + b**2))
 
-def ccw(A, B, C):
-    return (C[1] - A[1]) * (B[0] - A[0]) > (B[1] - A[1]) * (C[0] - A[0])
-
-def distance_on_line(p, s, f):
-	return np.dot(np.linalg.pinv((f - s).reshape((2, 1))), (s - p).reshape(2, 1)), np.dot(np.linalg.pinv((f - s).reshape((2, 1))), (f - p).reshape(2, 1))
 
 class Landmark:
     """This class represents a landmark in the robot's environment.
@@ -30,96 +25,17 @@ class Landmark:
     
     def update(self, other):
         """Update the landmark's parameters using a new observation"""
-        # assert self.equation[0] * other.equation[0] + self.equation[1] * other.equation[1] != 0
-        
         if self._r2 < other._r2:
             self._equation = other.equation
             self._start = other._start
             self._end = other._end
-
-        # if self.y_defined and other.y_defined or self.x_defined and other.x_defined:
-        #     self._equation = (self._equation * self._count + other.equation * other.count) / (self._count + other.count)
-        # elif self.y_defined:
-        #     print("!!!")
-        #     a, b, c = other.equation
-        #     self._equation = (self._equation * self._count + np.array((1/b, -1, -c/b)) * other.count) / (self._count + other.count)
-        # else:
-        #     print("!!!")
-        #     a, b, c = other.equation
-        #     self._equation = (self._equation * self._count + np.array((-1, 1/a, -c/a)) * other.count) / (self._count + other.count)
-        
         self._count += other.count
-        # other_start = self.closest_point(*other.start, False)
-        # other_end = self.closest_point(*other.end, False)
-        # if self.y_defined:
-        #     if other_start[0] < self._start[0]:
-        #         self._start = other_start
-        #     if other_end[0] > self._end[0]:
-        #         self._end = other_end
-        # elif self.x_defined:
-        #     if other_start[1] < self._start[1]:
-        #         self._start = other_start
-        #     if other_end[1] > self._end[1]:
-        #         self._end = other_end
         
     def closest_point(self, x, y, check_bounds=True):
         """Compute the closest point on the line to (`x`, `y`)"""
         p = np.array(closest_to_line(x, y, *self._equation))
-        # if not check_bounds:
         return p
-        d1, d2 = distance_on_line(p, self.start, self.end)
-        if np.sign(d1) != np.sign(d2):
-            return p
-        if np.sign(d1) <= 0:
-            return self.end
-        return self.start
-    
-    def distance(self, other, pos=(0, 0)):
-        """A measure of how different two landmarks are"""
-        if self.equation[1] != 0 and other.equation[1] != 0:
-            return pow((self.equation[0] / self.equation[1] - other.equation[0] / other.equation[1]) ** 2 + (self.equation[2] / self.equation[1] - other.equation[2] / other.equation[1]) , 0.5)
-        elif self.equation[0] != 0 and other.equation[0] != 0:
-            return pow((self.equation[1] / self.equation[0] - other.equation[1] / other.equation[0]) ** 2 + (self.equation[2] / self.equation[0] - other.equation[2] / other.equation[0]) , 0.5)
 
-    def intersects(self, other, threshold=0.3):
-        """Check if this landmark intersects with `other`."""
-        if self.equation[1] != 0:
-            self_d_vector = np.array([1, -self.equation[0]/self.equation[1]])
-        else:
-            self_d_vector = np.array([-self.equation[1]/self.equation[0], 1])
-        if other.equation[1] != 0:
-            other_d_vector = np.array([1, -other.equation[0]/other.equation[1]])
-        else:
-            other_d_vector = np.array([-other.equation[1]/other.equation[0], 1])
-        self_d_vector /= np.linalg.norm(self_d_vector)
-        other_d_vector /= np.linalg.norm(other_d_vector)
-        
-        self_start = self.start - threshold * self_d_vector
-        self_end = self.end + threshold * self_d_vector
-        other_start = other.start - threshold * other_d_vector
-        other_end = other.end + threshold * other_d_vector
-        
-        return all((
-            ccw(self_start, other_start, other_end) != ccw(self_end, other_start, other_end),
-            ccw(self_start, self_end, other_start) != ccw(self_start, self_end, other_end)
-        ))
-        # return any((
-        #     all((
-        #         any((
-        #             (other.start[0] - threshold < self.start[0] < other.end[0] + threshold),
-        #             (other.start[0] - threshold < self.end[0] < other.end[0] + threshold),
-        #             (self.start[0] - threshold < other.start[0] < self.end[0] + threshold),
-        #             (self.start[0] - threshold < other.end[0] < self.end[0] + threshold)
-        #         )),
-        #         any((
-        #             (other.start[1] - threshold < self.start[1] < other.end[1] + threshold),
-        #             (other.start[1] - threshold < self.end[1] < other.end[1] + threshold),
-        #             (self.start[1] - threshold < other.start[1] < self.end[1] + threshold),
-        #             (self.start[1] - threshold < other.end[1] < self.end[1] + threshold)
-        #         ))
-        #     )),
-        # ))
-        
     @property
     def y_defined(self):
         """True if the line can be described by `y = mx + d`"""
