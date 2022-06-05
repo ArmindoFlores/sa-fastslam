3c243bc2d469c4484303a7e8a2c3022bce66ab62
Tiago Lourinho <tiagolourinho001@gmail.com>
2022-06-05 18:20:30 +0100

    Tried to debug and find errors

--- catkin_workspace/src/fast_slam_node/scripts/fast_slam_node.py
+++ catkin_workspace/src/fast_slam_node/scripts/fast_slam_node.py
@@ -3,9 +3,11 @@ import rospy
 from std_msgs.msg import String
 import nav_msgs.msg
 import sensor_msgs.msg
-from auxiliar_scripts.particle_filter import *
-from auxiliar_scripts.landmark_extractor import  *
-from auxiliar_scripts.landmark_matching import  *
+import sys
+sys.path.insert(1, 'auxiliar_scripts')
+from particle_filter import *
+from landmark_extractor import  *
+from landmark_matching import  *
 import numpy as np
 
 
@@ -15,6 +17,7 @@ def H(xr, yr, tr):
     return np.array([[1, xr * np.sin(tr) - yr * np.cos(tr)], [0, 1]])
 
 def odom_callback(data):
+    global last_pose
     odom = {
         "header": {
             "seq": data.header.seq,
@@ -55,12 +58,16 @@ def odom_callback(data):
         }
     }
     
-    twist = odom["twist"]["twist"]
-    pos = twist["linear"]
-    rot = twist["angular"]
+    pos = odom["pose"]["pose"]["position"]
+    rot = odom["pose"]["pose"]["orientation"]
+    
+
+    pose = np.array([pos["x"], pos["y"], rot["z"]]) - last_pose
+    last_pose = pose
+
     # Update particles position if particle moved (needs to be changed to a more realistic threshold)
     if pos["x"] > 0 or pos["y"] > 0 or rot["z"] > 0:
-        pf.sample_pose(np.array([pos["x"], pos["y"], rot["z"]]), odom_covariance)
+        pf.sample_pose(pose, odom_covariance)
 
 def scan_callback(data):
     laser = {
@@ -80,11 +87,14 @@ def scan_callback(data):
         "intensities": data.intensities
     }
 
-    landmarks = extract_landmarks(laser["ranges"])
+    landmarks = extract_landmarks(laser)
 
     if len(landmarks) != 0:
         pf.observe_landmarks(landmarks, H, laser_covariance, Qt)
     
+    
+    for p in pf.particles:
+        print(p)
 
     pf.resample(pf.N)
 
@@ -95,11 +105,13 @@ def main():
     global odom_covariance
     global laser_covariance
     global Qt
+    global last_pose
 
     pf = ParticleFilter(10)
-    odom_covariance = np.array([0.0005, 0.0005, 0.0001])
+    odom_covariance = np.array([0.1, 0.1, 0.1])
     laser_covariance = 0.01 * np.identity(2)
     Qt = 0.01
+    last_pose = np.array([0, 0, 0])
 
 
     rospy.init_node('fast_slam_node', anonymous=True)
