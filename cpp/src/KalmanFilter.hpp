bb79d0284f9871029b55d88a351978b1ea474cb6
Francisco Rodrigues <francisco.rodrigues0908@gmail.com>
2022-11-29 14:43:50 +0000

    Updated documentation

--- cpp/src/KalmanFilter.hpp
+++ cpp/src/KalmanFilter.hpp
@@ -10,14 +10,39 @@
 */
 class KalmanFilter {
 public:
+    /*
+        Construct a new Kalman Filter
+
+        \param landmark the landmark to keep track of
+        \param covariance the initial 2x2 covariance matrix
+        \param Qt the 2x2 sensor covariance matrix
+    */
     KalmanFilter(const Landmark& landmark, cv::Mat covariance, cv::Mat Qt);
     KalmanFilter(const KalmanFilter&);
     KalmanFilter(KalmanFilter&&);
 
+    /*
+        \return The 2x2 covariance matrix associated with this landmark
+    */
     const cv::Mat& get_covariance() const;
+    /*
+        \return The 2x2 sensor covariance matrix
+    */
     const cv::Mat& get_Qt() const;
+    /*
+        \return A reference to the underlying landmark
+    */
     Landmark& get_landmark();
+    /*
+        \return A copy of the underlying landmark
+    */
     Landmark view_landmark() const;
+    /*
+        Update the covariance matrix and parameter estimations
+
+        \param measurement the observed parameters of the landmark
+        \param H the 2x2 Jacobian matrix of the measurement model matrix 
+    */
     void update(const cv::Vec2d& measurement, const cv::Mat& H);
 
     friend std::ostream& operator << (std::ostream&, const KalmanFilter&);
--- cpp/src/LandmarkMatcher.hpp
+++ cpp/src/LandmarkMatcher.hpp
@@ -7,23 +7,59 @@
 #include <vector>
 #include <opencv2/core.hpp>
 
+/*
+    This class is used to handle the matching of landmarks observed at different
+    times from different poses.
+*/
 class LandmarkMatcher {
 public:
+    /*
+        Create a new LandmarkMatcher object
+
+        \param Qt a 2x2 matrix representing sensor covariance
+        \param minimum_observations the minimum number of times a landmark must
+        be seen before it is confirmed as a real landmark
+        \param distance_threshold how close two landmarks have to be to be
+        considered the same one
+        \param max_invalid_landmarks the maximum number of invalid landmarks to
+        keep track of [NOT IMPLEMENTED!]
+    */
     LandmarkMatcher(cv::Mat Qt, int minimum_observations=6, double distance_threshold=0.3, int max_invalid_landmarks=-1);
     LandmarkMatcher(const LandmarkMatcher&);
     LandmarkMatcher(LandmarkMatcher&&);
     LandmarkMatcher& operator = (const LandmarkMatcher&);
     LandmarkMatcher& operator = (LandmarkMatcher&&);
     
+    /*
+        To be called when a new landmark is observed to check if it matches with
+        previously seen ones and to commit it to memory
+
+        \param landmark the newly seen landmark
+        \param H a function that returns the 2x2 Jacobian matrix of the measurement
+        model matrix
+        \param pose the robot's pose
+        \return The matching landmark, if found 
+    */
     std::optional<Landmark> observe(const Landmark& landmark, std::function<cv::Mat(double, double, double)> H, cv::Vec3d pose);
+    /*
+        \return A vector of all valid landmarks
+    */
     std::vector<Landmark> get_map() const;
+    /*
+        \return A vector of all previously seen landmarks
+    */
     std::vector<Landmark> get_full_map() const;
 
 private:
+    // Sensor covariance matrix
     cv::Mat Qt;
+    // Minimum number of observations before a landmark is considered valid
     int minimum_observations;
+    // Maximum distance for two landmarks to be considered the same
     double distance_threshold;
+    // Maximum number of invalid landmarks to keep track of (NOT IMPLEMENTED!)
     int max_invalid_landmarks;
+    // Extended Kalman Filters pertaining to all previously seen landmarks
     std::vector<std::pair<std::shared_ptr<KalmanFilter>, std::size_t>> filters;
 };
 
